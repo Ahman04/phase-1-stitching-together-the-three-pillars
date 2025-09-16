1c59c7945ab2a5b94a511d3cdfc5d1135c7b3619
Ahman04 <abokey2020@gmail.com>
2025-09-16 12:21:42 +0300

    work done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,2 @@
-require ( './helpers.js' );
 
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-  })
-})
+const testVar = {};
